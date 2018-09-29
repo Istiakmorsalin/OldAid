9c71c2c7ca4a8f03c4f060647dee6bb782717101
Tasnim Onisha <onisha.cse@gmail.com>
2018-09-29 21:35:36 +0600

    common web view updated

--- app/src/main/java/com/onisha/oldaid/webview/CommonWebViewActivity.java
+++ app/src/main/java/com/onisha/oldaid/webview/CommonWebViewActivity.java
@@ -1,4 +1,56 @@
 package com.onisha.oldaid.webview;
 
-public class CommonWebViewActivity {
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.onisha.oldaid.BaseActivity;
+import com.onisha.oldaid.R;
+
+
+public class CommonWebViewActivity extends BaseActivity {
+
+   public String url;
+
+    @Override
+    protected int layoutResId() {
+        return R.layout.common_web;
+    }
+
+    @Override
+    protected int menuResId() {
+        return 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //  setContentView(R.layout.activity_login);o
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String webUrl = getIntent().getStringExtra("URL");
+        webView.loadUrl(webUrl);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            private ProgressDialog mProgress;
+
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                if (mProgress == null) {
+                    mProgress = new ProgressDialog(CommonWebViewActivity.this);
+                    mProgress.show();
+                }
+                mProgress.setMessage("Loading " + String.valueOf(progress) + "%");
+                if (progress == 100) {
+                    mProgress.dismiss();
+                    mProgress = null;
+                }
+            }
+        });
+    }
 }
